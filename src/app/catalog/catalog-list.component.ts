1ae92725381a989f88192897df0adf8d0a521a84
duytanvn <duytan.cor.vn@gmail.com>
2019-12-03 03:28:23 +0100

    add interface type

--- src/app/catalog/catalog-list.component.ts
+++ src/app/catalog/catalog-list.component.ts
@@ -7,18 +7,39 @@ import { thisExpression } from 'babel-types';
 import * as _ from 'lodash';
 import { catalogData } from './catalog-data';
 
+export interface DescriptionField {
+  name?: string
+  label?: string
+  displayed?: boolean
+}
+
+export interface ProductItem {
+  image?: string
+  descriptionFieldList?: DescriptionField[]
+  descriptions?: any[]
+  richTextDescription?: string
+}
+
+export interface CatalogItem {
+  catalog?: string
+  name?: string
+  label?: string
+  images?: string[]
+  products?: ProductItem[]
+}
+
 @Component({
   selector: 'app-catalog-list',
   templateUrl: './catalog-list.component.html',
   styleUrls: ['./catalog.component.scss']
 })
- 
+
 export class CatalogListComponent implements OnInit {
   // tslint:disable-next-line: max-line-length
   public catalogItems: any[] = []
-  public currentCatalogItem: any
-  public currentProduct: any
-  public catalogData: any[] = []
+  public currentCatalogItem: CatalogItem
+  public currentProduct: ProductItem
+  public catalogData: CatalogItem[] = []
 
   @ViewChild('carousel', { static: false }) carousel: NgbCarousel;
 
@@ -33,7 +54,8 @@ export class CatalogListComponent implements OnInit {
       })
       return catalogItem
     })
-    this.catalogItems = catalogData
+    
+    this.catalogItems = this.catalogData
 
     this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
       const catalogName = paramMap.get('catalogName');
